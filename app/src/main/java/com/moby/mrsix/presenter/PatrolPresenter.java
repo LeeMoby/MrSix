22843f64c28cb10e73a5da312e368f5d890ff301
leemoby <leemoby.cn@gmail.com>
2016-03-30 13:39:10 +0800

    update MVP

--- app/src/main/java/com/moby/mrsix/presenter/PatrolPresenter.java
+++ app/src/main/java/com/moby/mrsix/presenter/PatrolPresenter.java
@@ -1,7 +1,12 @@
 package com.moby.mrsix.presenter;
 
+import com.moby.mrsix.model.IPatrolModel;
+import com.moby.mrsix.view.IIndexView;
+
 /**
  * Created by Moby on 16/3/29.
  */
 public class PatrolPresenter {
+    IPatrolModel iPatrolModel;
+    IIndexView iIndexView;
 }
